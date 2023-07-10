2457b39043f2d68291c7f725062d61ad06794060
Simon Viénot <simon.vienot@icloud.com>
2023-07-10 18:11:40 +0200

    Modify h5ai-nginx patch to use the SERVER_URL set in root .env file.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- scripts/hedera-apply-patch.sh
+++ scripts/hedera-apply-patch.sh
@@ -2,9 +2,11 @@
 
 SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 REPO_PATH=$( dirname "${SCRIPTS_PATH}")
+ENVIRONMENTS_PATH="${REPO_PATH}/environments"
 
 echo 'Patching /h5ai-nginx/select-contract-form'
-cp ${REPO_PATH}/h5ai-nginx-patch/.env ${REPO_PATH}/h5ai-nginx/select-contract-form/
-cd ${REPO_PATH}/h5ai-nginx
-git apply ../h5ai-nginx-patch/select-contract-form.patch
+TARGET_URL=$(. ${ENVIRONMENTS_PATH}/.env; printf '%s' "$SERVER_URL")
+echo "REACT_APP_SERVER_URL=${TARGET_URL}" > ${REPO_PATH}/h5ai-nginx/select-contract-form/.env
+#cd ${REPO_PATH}/h5ai-nginx
+#git apply ../h5ai-nginx-patch/select-contract-form.patch
 
