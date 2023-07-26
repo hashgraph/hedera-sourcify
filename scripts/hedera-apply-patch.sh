d59cb32a7978e2f0f345de7e1a393158186767c2
Simon Viénot <simon.vienot@icloud.com>
2023-07-26 12:56:13 +0200

    Update h5ai-nginx patch.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- scripts/hedera-apply-patch.sh
+++ scripts/hedera-apply-patch.sh
@@ -5,8 +5,8 @@ REPO_PATH=$( dirname "${SCRIPTS_PATH}")
 ENVIRONMENTS_PATH="${REPO_PATH}/environments"
 
 echo 'Patching /h5ai-nginx/select-contract-form'
-TARGET_URL=$(. ${ENVIRONMENTS_PATH}/.env; printf '%s' "$SERVER_URL")
-echo "REACT_APP_SERVER_URL=${TARGET_URL}" > ${REPO_PATH}/h5ai-nginx/select-contract-form/.env
-#cd ${REPO_PATH}/h5ai-nginx
-#git apply ../h5ai-nginx-patch/select-contract-form.patch
+#TARGET_URL=$(. ${ENVIRONMENTS_PATH}/.env; printf '%s' "$SERVER_URL")
+#echo "REACT_APP_SERVER_URL=${TARGET_URL}" > ${REPO_PATH}/h5ai-nginx/select-contract-form/.env
+cd ${REPO_PATH}/h5ai-nginx
+git apply ../h5ai-nginx-patch/select-contract-form.patch
 
