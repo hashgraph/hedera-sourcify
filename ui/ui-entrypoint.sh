544d717c9a46ecdb81162bac8610ecd23e6116d2
Simon Viénot <simon.vienot@icloud.com>
2023-09-01 13:41:35 +0200

    Clean-up (now unused) docker entry point for ui.
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- ui/ui-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Patch index.html to insert config parameter in the global window object
-sed -i "s@<script></script>@<script>window.configs={SERVER_URL:\"${SERVER_URL}\",REPOSITORY_SERVER_URL:\"${REPOSITORY_SERVER_URL}\"}</script>@" /usr/share/nginx/html/index.html
-
-# Start nginx
-nginx -g "daemon off;"
