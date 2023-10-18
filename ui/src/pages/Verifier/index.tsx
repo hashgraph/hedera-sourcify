19dd5a29d76a946983d9b6494c41547e96529c95
Simon Viénot <simon.vienot@icloud.com>
2023-10-18 13:30:55 +0200

    Display link to terms-of-service only when URL provided in configuration
    Signed-off-by: Simon Viénot <simon.vienot@icloud.com>

--- ui/src/pages/Verifier/index.tsx
+++ ui/src/pages/Verifier/index.tsx
@@ -2,7 +2,7 @@ import bytes from "bytes";
 import { useCallback, useContext, useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Toast from "../../components/Toast";
-import { DOCS_URL, TERMS_OF_SERVICE_URL } from "../../constants";
+import { DOCS_URL } from "../../constants";
 import { Context } from "../../Context";
 import {
   Create2VerificationInput,
@@ -245,7 +245,9 @@ const Verifier: React.FC = () => {
       </div>
       <div className="text-center text-xs italic mx-2 mt-1 text-gray-400">
         <p>Note: Once a contract is verified it can't be removed from the repository.</p>
-        <a href={TERMS_OF_SERVICE_URL}>See Terms of Service</a>
+        {configuration.termsOfServiceUrl && (
+          <a href={configuration.termsOfServiceUrl}>See Terms of Service</a>
+        )}
       </div>
     </div>
   );
