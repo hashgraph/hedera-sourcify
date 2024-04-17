78c4de96a43185c0f30332abf400b2ffa00a55fe
Luis Mastrangelo <luis@swirldslabs.com>
2024-04-17 21:09:30 +0200

    Remove more unused files
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- scripts/updateChains.mjs
+++
@@ -1,9 +0,0 @@
-import { promises } from "fs";
-
-const chainsUrl = "https://chainid.network/chains.json";
-const chainsPath = "src/chains.json";
-
-const result = await fetch(chainsUrl);
-const chainsList = await result.text();
-
-await promises.writeFile(chainsPath, chainsList);
