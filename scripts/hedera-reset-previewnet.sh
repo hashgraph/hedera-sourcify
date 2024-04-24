fbbbd17d7de0e45b1bb7ceed7570e2d48c404ce2
Luis Mastrangelo <acuarica@users.noreply.github.com>
2024-04-24 12:31:01 -0300

    chore: remove unused config files (#159)
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- scripts/hedera-reset-previewnet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Resets the contract verifications for previewnet by removing the corresponding directories
-# in the repository.
-# Assumes that the services (server, ui, repository) are shutdown
-#
-
-SCRIPTS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${SCRIPTS_PATH}/hedera-reset.sh previewnet
