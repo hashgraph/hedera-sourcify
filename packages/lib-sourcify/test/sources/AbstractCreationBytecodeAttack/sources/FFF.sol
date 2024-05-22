1e94e28f4c1c389d32803f902c19eeb9dfc3c357
Luis Mastrangelo <luis@swirldslabs.com>
2024-05-22 15:31:05 +0200

    Remove soon to be obsolete `packages` and `src` folders
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- packages/lib-sourcify/test/sources/AbstractCreationBytecodeAttack/sources/FFF.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.6.12;
-
-abstract contract FFF{
-    constructor (uint256 a) public payable {
-    }
-}
--- packages/lib-sourcify/test/sources/WithImmutablesCreationBytecodeAttack/sources/WithImmutables.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity >=0.7.0;
-
-contract WithImmutables{
-    constructor (uint256 a) {
-    }
-}
