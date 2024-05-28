9810d773fca1d69942617356ac88d1c668b5c6e7
Luis Mastrangelo <acuarica@users.noreply.github.com>
2024-05-28 14:50:10 -0300

    chore: remove soon to be obsolete `packages` and `src` folders (#170)
    Signed-off-by: Luis Mastrangelo <luis@swirldslabs.com>

--- src/common/errors/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./NotFoundError";
-export * from "./ValidationError";
-export * from "./InternalServerError";
-export * from "./BadRequestError";
-export * from "./PayloadTooLargeError";
--- src/server/services/services.ts
+++
@@ -1,9 +0,0 @@
-import config from "../../config";
-import { supportedChainsMap } from "../../sourcify-chains";
-import { RepositoryService } from "./RepositoryService";
-import { VerificationService } from "./VerificationService";
-
-export const services = {
-  verification: new VerificationService(supportedChainsMap),
-  repository: new RepositoryService(config.repository.path),
-};
